3c07eecaffe7f5a2508e71b031e0a59b2bf22e7c
Handsken <nan.222@yandex.ru>
2022-12-19 13:12:05 +0500

    first

--- src/Author.java
+++ src/Author.java
@@ -1,23 +1,44 @@
+import java.util.Objects;
+
 public class Author {
     private String firstname;
     private String lastname;
+
     public Author(String firstname, String lastname) {
         this.firstname = firstname;
         this.lastname = lastname;
     }
+
     public String getFirstname() {
         return this.firstname;
     }
+
     public String getLastname() {
         return this.lastname;
     }
-    public String setFristname(String firstname){
+
+    public String setFristname(String firstname) {
         return this.firstname;
     }
-    public String setLastname(String lastname){
+
+    public String setLastname(String lastname) {
         return this.lastname;
     }
-    public String toString(){
-        return firstname+" "+lastname;
+
+    public String toString() {
+        return firstname + " " + lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return firstname.equals(author.firstname) && lastname.equals(author.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstname, lastname);
     }
 }
